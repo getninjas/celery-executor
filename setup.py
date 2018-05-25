21793b437e5776d8e953f203f00aa93089379c92
Alan Justino <alan.justino@yahoo.com.br>
2018-05-25 16:38:35 -0300

    README is Markdown

--- setup.py
+++ setup.py
@@ -6,15 +6,17 @@
 import sys
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.md') as history_file:
     history = history_file.read()
 
 requirements = [ ]
 
-setup_requirements = [ ]
+setup_requirements = [
+    'setuptools>=38.6.0',
+]
 
 test_requirements = ['pytest', ]
 
@@ -38,6 +40,7 @@ setup(
     install_requires=requirements,
     license="Apache Software License 2.0",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',  # This is important!
     include_package_data=True,
     keywords='celery_executor',
     name='celery-executor',
