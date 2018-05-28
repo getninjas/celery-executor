39c399de2592a119bbc0b15c2526676b7574161c
Alan Justino <alan.justino@yahoo.com.br>
2018-05-28 19:33:31 -0300

    CeleryExecutor futures should behave as common Future instances

--- tests/test_celery_executor.py
+++ tests/test_celery_executor.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `celery_executor` package."""
+import time
+from pprint import pformat
+from concurrent.futures import ThreadPoolExecutor
 
 import pytest
 
-
 from celery_executor.executors import CeleryExecutor, SyncExecutor
-from concurrent.futures import ThreadPoolExecutor
 
 
 @pytest.fixture(scope='session')
@@ -19,6 +20,12 @@ def celery_config():
         # See: https://github.com/celery/celery/issues/3586
         # and https://github.com/celery/celery/pull/3592
         'result_serializer': 'pickle',
+
+        'worker_concurrency': 1,
+        'worker_prefetch_multiplier': 1,
+        'worker_lost_wait': 60,
+        'worker_send_task_events': True,
+        'task_acks_late': True,
     }
 
 
@@ -55,3 +62,64 @@ def test_excutor_exception_parity(celery_session_worker):
 
     with pytest.raises(OSError):
         list(cl_exec.map(open, operations))
+
+
+def test_futures_parity(celery_session_worker):
+    tp_exec = ThreadPoolExecutor(max_workers=1)
+    cel_exec = CeleryExecutor()
+    # SyncExecutor Futures will not be tested as they are standard Python ones.
+
+    operations = [2, 3]  # The last will be canceled.
+    func = time.sleep
+
+    tp_futures = []
+    cel_futures = []
+
+    for op in operations:
+        tp_futures.append(tp_exec.submit(func, op))
+        cel_futures.append(cel_exec.submit(func, op))
+
+    time.sleep(10)   # First should had finished. Second no.
+
+    states = []
+    assert len(tp_futures) == len(cel_futures)
+    for tp_fut, cel_fut in zip(tp_futures, cel_futures):
+        states.append(  # A tuple with (tp, cel) results
+            (_collect_state(tp_fut),
+             _collect_state(cel_fut)),
+        )
+
+    for tp_fut_state, cel_fut_state in states:
+        assert pformat(tp_fut_state) == pformat(cel_fut_state)
+
+    states = []
+    for tp_fut, cel_fut in zip(tp_futures, cel_futures):
+        tp_cancel_result = tp_fut.cancel()
+        cel_cancel_result = cel_fut.cancel()
+        states.append(  # A tuple with (tp, cel) results
+            ((tp_cancel_result, _collect_state(tp_fut)),
+             (cel_cancel_result, _collect_state(cel_fut))),
+        )
+
+    for tp_fut_state, cel_fut_state in states:
+        assert tp_fut_state == cel_fut_state
+
+
+def _collect_state(future):
+    state = {
+        'cancelled': future.cancelled(),
+        'running': future.running(),
+        'done': future.done(),
+    }
+    try:
+        state['result'] = future.result(timeout=0)
+    except Exception as err:
+        state['result'] = err
+
+    try:
+        state['exception'] = future.exception(timeout=0)
+    except Exception as err:
+        state['exception'] = err
+
+    state['_state'] = future._state
+    return state
