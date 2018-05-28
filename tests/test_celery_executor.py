10adf20d8b5df1db249a922021d7b2100b518a38
Alan Justino <alan.justino@yahoo.com.br>
2018-05-28 12:35:08 -0300

    Minimal tests executors parity

--- tests/test_celery_executor.py
+++ tests/test_celery_executor.py
@@ -6,7 +6,8 @@
 import pytest
 
 
-from celery_executor.executors import CeleryExecutor
+from celery_executor.executors import CeleryExecutor, SyncExecutor
+from concurrent.futures import ThreadPoolExecutor
 
 
 @pytest.fixture
@@ -23,3 +24,38 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+
+def test_excutors_parity():
+    tp_exec = ThreadPoolExecutor()
+    s_exec = SyncExecutor()
+    cl_exec = CeleryExecutor()
+
+    operations = ['first', 'second']
+
+    map_results = list(sorted(map(str.upper, operations)))
+    s_results = list(sorted(s_exec.map(str.upper, operations)))
+    tp_results = list(sorted(tp_exec.map(str.upper, operations)))
+    # cl_results = list(sorted(cl_exec.map(str.upper, operations)))
+
+    assert map_results == s_results == tp_results # == cl_results
+
+
+def test_excutor_exception_parity():
+    tp_exec = ThreadPoolExecutor()
+    s_exec = SyncExecutor()
+    cl_exec = CeleryExecutor()
+
+    operations = ['/nonexistentfile', '/anothernonexistentfile']
+
+    with pytest.raises(OSError):
+        list(map(open, operations))
+
+    with pytest.raises(OSError):
+        list(tp_exec.map(open, operations))
+
+    with pytest.raises(OSError):
+        list(s_exec.map(open, operations))
+
+    # with pytest.raises(OSError):
+    #     list(cl_exec.map(open, operations))
